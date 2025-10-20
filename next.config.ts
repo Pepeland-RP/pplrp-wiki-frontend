b6e3d32a1c45e2cbcbd3bc3d1dc64cd0abffe1a8
Andcool-Systems <drolll2007@gmail.com>
2025-10-21 00:32:27 +0300

    refactor: Disabled react strict mode

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Технически, стрикт мод нужен, чтобы помочь неопытным разрабам не насрать в прод, но мы же не такие))
+  reactStrictMode: false,
 };
 
 export default nextConfig;
