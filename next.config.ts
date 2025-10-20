94a4be18ebb8fa63a15dd9986b740fc03f9c4dd4
Andcool-Systems <drolll2007@gmail.com>
2025-10-21 00:34:02 +0300

    feat(next config): Added configuration for inline svg importing (@svgr/webpack)

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,24 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   // Технически, стрикт мод нужен, чтобы помочь неопытным разрабам не насрать в прод, но мы же не такие))
   reactStrictMode: false,
+
+  webpack(config) {
+    config.cache = false;
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
+  },
 };
 
 export default nextConfig;
