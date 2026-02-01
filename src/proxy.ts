f3b352d6e068995b5b6560c1e62e26af98d03beb
Andcool-Systems <drolll2007@gmail.com>
2026-02-01 17:27:29 +0300

    fix(env): update API URL configuration and ensure correct proxy handling for asset requests

--- src/proxy.ts
+++ src/proxy.ts
@@ -5,6 +5,12 @@ export function proxy(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
   requestHeaders.set('X-Path', url.pathname);
 
+  if (url.pathname.startsWith('/api/assets/')) {
+    return NextResponse.rewrite(
+      `${process.env.PRIVATE_API_URL}${url.pathname.replace('/api', '')}`,
+    );
+  }
+
   return NextResponse.next({
     request: {
       headers: requestHeaders,
